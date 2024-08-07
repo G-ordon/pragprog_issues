da5905dbbe1ee8b2ed0d7c05a3956a74e22f22f3
Gordon Ochieng <gordonochieng5454@gmail.com>
2024-08-07 15:54:26 +0300

    Parsing the Command Line
    We are putting our CLI.ex in the lib directory. hense we create for it a subdirectory with the same name as a project.

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,31 @@
+defmodule Issues.GithubIssues do
+  @user_agent [ { "User-agent", "Elixir dave@progprrag.com"} ]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  # use a module attribute to fetch the value at compile time
+  @github_url Application.compile_env(:issues, :github_url)
+  def issues_url(user, project) do
+  "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+  def handle_response({ :ok, %{status_code: 200, body: body}}) do
+    { :ok,   body}
+  end
+  def handle_response({ _,  %{status_code: _,    body: body}}) do
+    { :error, body}
+  end
+  def handle_response({_, %{status_code: status_code, body: body}}) do
+  {
+    status_code |> check_for_error(),
+    body        |> Poison.Parser.parse!()
+  }
+  end
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_),   do: :error
+
+
+end
